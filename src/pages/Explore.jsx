8052a475ed5bdea977e1fccc376790fb738bf2c1
Your Name <you@example.com>
2025-09-08 11:10:52 -0600

    hotcollections carousel

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -6,7 +6,7 @@ const Explore = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-//Does this work?
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
