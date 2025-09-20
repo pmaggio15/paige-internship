400db0923428b57bf106e29572b38a935b559c50
Your Name <you@example.com>
2025-09-19 19:43:53 -0600

    animations

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import SubHeader from "../images/subheader.jpg";
 import ExploreItems from "../components/explore/ExploreItems";
+import AOS from 'aos';
 
 const Explore = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
+    AOS.refresh(); 
   }, []);
 
   return (
@@ -16,12 +18,13 @@ const Explore = () => {
           id="subheader"
           className="text-light"
           style={{ background: `url("${SubHeader}") top` }}
+          data-aos="fade-down"
         >
           <div className="center-y relative text-center">
             <div className="container">
               <div className="row">
                 <div className="col-md-12 text-center">
-                  <h1>Explore</h1>
+                  <h1 data-aos="fade-up" data-aos-delay="200">Explore</h1>
                 </div>
                 <div className="clearfix"></div>
               </div>
@@ -29,7 +32,7 @@ const Explore = () => {
           </div>
         </section>
 
-        <section aria-label="section">
+        <section aria-label="section" data-aos="fade-up" data-aos-delay="300">
           <div className="container">
             <div className="row">
               <ExploreItems />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,22 +5,36 @@ import Landing from "../components/home/Landing";
 import LandingIntro from "../components/home/LandingIntro";
 import NewItems from "../components/home/NewItems";
 import TopSellers from "../components/home/TopSellers";
+import AOS from 'aos';
 
 const Home = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
+    AOS.refresh();
   }, []);
 
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
-        <Landing />
-        <LandingIntro />
-        <HotCollections />
-        <NewItems />
-        <TopSellers />
-        <BrowseByCategory />
+        <div data-aos="fade-in">
+          <Landing />
+        </div>
+        <div data-aos="fade-up" data-aos-delay="200">
+          <LandingIntro />
+        </div>
+        <div data-aos="fade-up" data-aos-delay="300">
+          <HotCollections />
+        </div>
+        <div data-aos="fade-up" data-aos-delay="400">
+          <NewItems />
+        </div>
+        <div data-aos="fade-up" data-aos-delay="500">
+          <TopSellers />
+        </div>
+        <div data-aos="fade-up" data-aos-delay="600">
+          <BrowseByCategory />
+        </div>
       </div>
     </div>
   );
